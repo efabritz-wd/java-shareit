955ef126cbf3306fbc6b1d1720a2fec3467102d5
efabritz <fabritzekaterina@yandex.com>
2025-09-17 15:38:46 +0200

    checkstyle corrections

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
